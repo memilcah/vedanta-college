7bc8264324874bffd44c72521a2ff36ce647911d
memilcah <greatymilcah@gmail.com>
2026-03-05 14:14:53 +0530

    Add files via upload

---
+++ blogs.js
@@ -0,0 +1,125 @@
+// acript js
+function changeReadMore1() {
+    const mycontent =
+        document.getElementById('mybox1id1');
+    const mybutton =
+        document.getElementById('mybuttonid1');
+    const span1 = document.getElementById("span1")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span1.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span1.style.display = "inline";
+    }
+}
+
+
+// acript js
+function changeReadMore2() {
+    const mycontent =
+        document.getElementById('mybox1id2');
+    const mybutton =
+        document.getElementById('mybuttonid2');
+    const span2 = document.getElementById("span2")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span2.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span2.style.display = "inline";
+    }
+}
+
+
+// acript js
+function changeReadMore3() {
+    const mycontent =
+        document.getElementById('mybox1id3');
+    const mybutton =
+        document.getElementById('mybuttonid3');
+    const span3 = document.getElementById("span3")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span3.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span3.style.display = "inline";
+    }
+}
+
+
+
+// acript js
+function changeReadMore4() {
+    const mycontent =
+        document.getElementById('mybox1id4');
+    const mybutton =
+        document.getElementById('mybuttonid4');
+    const span3 = document.getElementById("span4")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span4.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span4.style.display = "inline";
+    }
+}
+
+
+// acript js
+function changeReadMore5() {
+    const mycontent =
+        document.getElementById('mybox1id5');
+    const mybutton =
+        document.getElementById('mybuttonid5');
+    const span3 = document.getElementById("span5")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span5.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span5.style.display = "inline";
+    }
+}
+
+
+// acript js
+function changeReadMore6() {
+    const mycontent =
+        document.getElementById('mybox1id6');
+    const mybutton =
+        document.getElementById('mybuttonid6');
+    const span3 = document.getElementById("span6")
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        span6.style.display = "none";
+        mybutton.textContent = 'Read Less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'Read More';
+        span6.style.display = "inline";
+    }
+}
